11455b228165997238c201b077c570ed8c25bd73
Moses Aizee <maizee@twilio.com>
2023-04-26 11:36:29 +0100

    testing

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 # Validation
 
-## Development server
 
 Run `nx serve validation` for a dev server. Navigate to http://localhost:4200/. The app will automatically reload if you change any of the source files.
 
--- src/components/app/app.spec.tsx
+++ src/components/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/My new app/gi)).toBeTruthy();
+    expect(getByText(/My new apps/gi)).toBeTruthy();
   });
 });
